5a39b2a1f53284d759fe5f7a6a63b086b2326580
Riley <uyjustinriley@gmail.com>
2021-11-24 18:18:37 +0800

    Fixing article item

--- components/ArticleItem/ArticleItem.js
+++ components/ArticleItem/ArticleItem.js
@@ -7,23 +7,47 @@ import eventPicture from '../../public/images/temp-event.png'
 
 const ArticleItem = () => {
     return (
-        <div className={`${styles.container} py-4 mr-16 w-72`}>
-            <div className="relative w-full h-48">
-                <Image
-                    src={eventPicture}
-                    alt="Picture of Event"
-                    layout="fill"
-                    objectPosition="left"
-                    objectFit="cover"
-                />
+        // <div className={`${styles.container} py-4 mr-16 w-72`}>
+        //     <div className="relative pd">
+        //             <div className="w-96 h-72">
+        //                 <Image
+        //                         src={eventPicture}
+        //                         alt="Placeholder"
+        //                         layout="fill"
+        //                         objectFit="contain"
+        //                         objectPosition="center"
+        //                     />
+        //             </div>
+        //         </div>
+        //     <p className="font-bold text-lg w-full text-justify"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit </p>
+        //     <p className="text-base w-full text-justify"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit </p>
+        //     <p className="text-base w-full text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+        //                                                 ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+        //                                                 laboris nisi ut aliquip ex ea commodo consequat.</p>
+        // </div>
+        <div className="px-4 sm:px-32 flex flex-col md:flex-row">
+            <div className="relative pd">
+                <div className="w-96 h-56">
+                    <Image
+                        src={eventPicture}
+                        alt="Placeholder"
+                        layout="fill"
+                        objectFit="contain"
+                        objectPosition="center"
+                    />
+                </div>
             </div>
-            <p className="font-bold text-lg w-full text-justify"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit </p>
-            <p className="text-base w-full text-justify"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit </p>
-            <p className="text-base w-full text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                                                        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                                                        laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+            <div className=" md:pl-16 lg:pl-74">
+                <h1 className="mb-8 sm:pd-2">Lorem Ipsum Dolor</h1>
+                <p className="whitespace-nowrap">July 9-12, 2019 · 08:00am - 05:00pm</p>
+                <p className="pb-8 ">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                    sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                    magna aliquam erat volutpat. Ut wisi </p>
+            </div>
+
         </div>
     );
 };
 
-export default EventItem;
+export default ArticleItem;
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from "next/image";
 import Layout from "../../components/layout";
 import ArticleItem from '../../components/ArticleItem/ArticleItem';
-import DateTabs from '../../components/DateTabs/DateTabs';
 import SEO from "../../components/seo";
 import styles from "../../styles/Home.module.css";
 import eventPicture from '../../public/images/temp-event.png'
@@ -11,15 +10,15 @@ import { useEffect, useState } from "react";
 export default function Events() {
     return (
         <Layout active={4}>
-            <SEO title={"Events"} />
+            <SEO title={"Articles"} />
 
-            <h1 className="pl-32 py-12">Articles</h1>
+            <h1 className="pl-32 py-24">Articles</h1>
 
             {/* <section>
                 <DateTabs/>
             </section> */}
 
-            <section className="px-4 sm:px-32 flex flex-column justify-start w-full md:w-4/5">
+            <section className="px-4 sm:px-32 flex flex-wrap justify-start w-full md:w-4/5">
                 <ArticleItem/>
                 <ArticleItem/>
                 <ArticleItem/>
