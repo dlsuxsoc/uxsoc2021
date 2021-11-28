dad72caee201a666d2112941b0338edde4e4bd21
Riley <uyjustinriley@gmail.com>
2021-11-28 15:19:09 +0800

    Articles page formatting and date tab formatting

--- components/ArticleItem/ArticleItem.js
+++ components/ArticleItem/ArticleItem.js
@@ -3,55 +3,16 @@ import styles from "./ArticleItem.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 import eventPicture from '../../public/images/temp-event.png'
+import faker from 'faker';
 
 
-const ArticleItem = () => {
+const ArticleItem = ({title, description, date}) => {
     return (
-        // <div className={`${styles.container} py-4 mr-16 w-72`}>
-        //     <div className="relative pd">
-        //             <div className="w-96 h-72">
-        //                 <Image
-        //                         src={eventPicture}
-        //                         alt="Placeholder"
-        //                         layout="fill"
-        //                         objectFit="contain"
-        //                         objectPosition="center"
-        //                     />
-        //             </div>
-        //         </div>
-        //     <p className="font-bold text-lg w-full text-justify"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit </p>
-        //     <p className="text-base w-full text-justify"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit </p>
-        //     <p className="text-base w-full text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-        //                                                 ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-        //                                                 laboris nisi ut aliquip ex ea commodo consequat.</p>
-        // </div>
-        // <div className="px-4 sm:px-32 flex flex-col md:flex-row w-4/5">
-        //     <div className="relative pd">
-        //         <div className="w-96 h-56">
-        //             <Image
-        //                 src={eventPicture}
-        //                 alt="Placeholder"
-        //                 layout="fill"
-        //                 objectFit="contain"
-        //                 objectPosition="center"
-        //             />
-        //         </div>
-        //     </div>
-
-        //     <div className=" md:pl-16 lg:pl-74">
-        //         <h1 className="mb-8 sm:pd-2">Lorem Ipsum Dolor</h1>
-        //         <p className="whitespace-nowrap">July 9-12, 2019 · 08:00am - 05:00pm</p>
-        //         <p className="pb-8 ">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-        //             sed diam nonummy nibh euismod tincidunt ut laoreet dolore
-        //             magna aliquam erat volutpat. Ut wisi </p>
-        //     </div>
-
-        // </div>
-        <section className="px-4 sm:px-32 py-2 mb-16 lg:mb-36 flex flex-col lg:flex-row justify-center lg:justify-between items-center h-auto">
+        <section className="px-4 sm:px-32 py-2 mb-16 lg:mb-28 flex flex-col lg:flex-row justify-center lg:justify-between h-auto">
             {/* Image */}
             <div className="w-full lg:w-1/2 pb-2 pr-0 lg:pr-20">
                 <Image
-                    src={eventPicture}
+                    src={faker.animal.dog()}
                     alt="Placeholder"
                     width={489}
                     height={311}
@@ -59,14 +20,10 @@ const ArticleItem = () => {
             </div>
 
             {/* Header and Text */}
-            <div className="w-full lg:w-1/2 pb-3 lg:pb-0">
-                <h1 className="text-black text-3xl lg:text-5xl mb-6 lg:mb-12">Lorem ipsum dolor</h1>
-                <p className="text-sm lg:text-base mb-4">
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit 
-                </p>
-                <p className="text-sm lg:text-base mb-4">
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit 
-                </p>
+            <div className="w-full lg:w-1/2  lg:pb-0">
+                <h1 className="text-black text-3xl lg:text-4xl mb-6 lg:mb-12">{title}</h1>
+                <p className="text-sm lg:text-base mb-4">{date}</p>
+                <p className="text-sm lg:text-base mb-4">{description}</p>
             </div>
         </section>
     );
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -9,27 +9,76 @@ import styles from "../../styles/Home.module.css";
 import eventPicture from '../../public/images/temp-event.png'
 import { useEffect, useState } from "react";
 
-export default function Events({ active, year }) {
+export default function Articles({ active }) {
+    const newArticles = [
+        {
+            date: "11/12/2019",
+            title: "UXSOC starts fresh with an Intro to UX talk",
+            description: "Along with UXSOC DLSU's new set of officers for A.Y 2021-2022, they are set to bring the org from the ground up.",
+        },
+        {
+            date: "1/28/2022",
+            title: "Accessibility Talks",
+            description: "Learn more about accessbility in UX.",
+        },
+        {
+            date: "11/12/2021",
+            title: "State of UX Philippines",
+            description: "State of the UX, November 2021 Edition.",
+        },
+        {
+            date: "12/10/2021",
+            title: "Intro to UX",
+            description: "An event for UX beginners.",
+        },
+        {
+            date: "1/24/2020",
+            title: "UX Career Shifting",
+            description: "Learn about switching to a UX Career!",
+        },
+        {
+            date: "6/4/2022",
+            title: "UXSOC Taft - Rebranding Event",
+            description: "DLSU UXSOC is now UXSOC Taft!",
+        },
+    ];
+
+    const [year, setYear] = useState("All"); // selected date
+    const [articles, setArticles] = useState(newArticles);
+
+    useEffect(() => {
+        setArticles(
+            newArticles.filter((item) => {
+                return item.date.split("/")[2] === year || year === "All";
+            })
+        );
+    }, [year]); // will run for every change ng dependency array
+
+
     return (
         <Layout active={4}>
             <SEO title={"Articles"} />
 
             <h1 className="pl-32 lg:pl-32 py-24">Articles</h1>
 
-
-
-            <section className="flex flex-row">
-                <section className="md:flex-row-reverse">
-                    <ArticleItem />
-                    <ArticleItem />
-                    <ArticleItem />
-                </section>
+            <section className="flex flex-col-reverse lg:flex-row">
+                <ul className="md:flex-row-reverse">
+                    {newArticles.map(({ title, description, date }, index) => (
+                        <ArticleItem key={index} title={title} description={description} date={date}/>
+                    ))}
+                </ul>
 
                 <section className="w-16 pr-5 pt-8">
-                    {/* <DateTabs setEventItems={setEventItems} events={events} /> */}
                     {data.map((item, index) => (
-                        <DateTabs active={index == active} year={item.year} />
+                        <DateTabs
+                            key={index}
+                            year={item.year}
+                            active={item.year === year}
+                            set={setYear}
+                        />
                     ))}
+
+
                 </section>
             </section>
         </Layout>
