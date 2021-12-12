ecaaf23744bb88b957929a7ecfe2fcf8b109e633
Tyrone Sta. Maria <tyrone.stamaria@orangeandbronze.com>
2021-12-12 14:38:36 +0800

    connected events page to contentful

--- components/EventItem/EventItem.js
+++ components/EventItem/EventItem.js
@@ -2,25 +2,28 @@ import React from "react";
 import styles from "./EventItem.module.scss";
 import Image from "next/image";
 import Link from "next/link";
-import eventPicture from '../../public/images/temp-event.png'
-import faker from 'faker';
+import eventPicture from "../../public/images/temp-event.png";
+import faker from "faker";
 
-const EventItem = ({title, description}) => {
-    return (
-        <li className={`py-4 mr-0 md:mr-16 w-72`}>
-            <div className="relative w-full h-56">
-                <Image
-                    src={faker.image.image()}
-                    alt="Picture of Event"
-                    layout="fill"
-                    objectPosition="center"
-                    objectFit="contain"
-                />
-            </div>
-            <p className="font-bold text-lg w-full text-justify pt-6 pb-2"> {title} </p>
-            <p className="text-base w-full text-justify"> {description} </p>
-        </li>
-    );
+const EventItem = ({ title, description }) => {
+  return (
+    <li className={`py-4 mr-0 md:mr-16 w-72`}>
+      <div className="relative w-full h-56">
+        <Image
+          src={faker.image.image()}
+          alt={title}
+          layout="fill"
+          objectPosition="center"
+          objectFit="contain"
+        />
+      </div>
+      <p className="font-bold text-lg w-full text-justify pt-6 pb-2">
+        {" "}
+        {title}{" "}
+      </p>
+      {/* <p className="text-base w-full text-justify"> {description} </p> */}
+    </li>
+  );
 };
 
 export default EventItem;
