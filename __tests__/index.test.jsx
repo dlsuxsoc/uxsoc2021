564c849fa74224b78441a0178d6724e6f58ab13c
symatt <jamesxsy17@gmail.com>
2021-12-12 22:45:48 +0800

    Update index.test.jsx

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -35,3 +35,60 @@
 //     expect(heading).toBeInTheDocument()
 //   })
  })
+
+ // content of articles is seen in the articles page
+ describe('Get articles positive', () => {
+
+    it('renders article 1\'s content in the articles page.', () => {
+        const article = {
+            content: { json: { noteType: 'document', data: {}, content: [
+                { nodeType: 'paragraph', content: [ {
+                    "nodeType": "text",
+                    "value": "bla bla bla",
+                  } ], data: {} }
+              ] } },
+            authors: null,
+            title: "Article 1",
+            slug: "article-1",
+        };
+        const article_content = getRTFContent(article);
+        
+        expect(article_content).toEqual("bla bla bla");
+     })
+
+     it('renders article 2\'s content in the articles page.', () => {
+        const article = {
+            content: { json: { noteType: 'document', data: {}, content: [
+                { nodeType: 'paragraph', content: [ {
+                    "nodeType": "text",
+                    "value": "bla bla bla",
+                  } ], data: {} }
+              ] } },
+            authors: null,
+            title: "Article 2",
+            slug: "article-2",
+            imagesCollection: { items: [ [Object], [Object] ] }
+        };
+        const article_content = getRTFContent(article, 0, 5);
+        
+        expect(article_content).toEqual("bla b");
+     })
+
+     it('renders article 3\'s content in the articles page.', () => {
+        const article = {
+            content: { json: { noteType: 'document', data: {}, content: [
+                { nodeType: 'paragraph', content: [ {
+                    "nodeType": "text",
+                    "value": "bla bla bla",
+                  } ], data: {} }
+              ] } },
+            authors: null,
+            title: "Article 3",
+            slug: "article-3",
+            imagesCollection: { items: [ [Object], [Object] ] }
+        };
+        const article_content = getRTFContent(article, 1, 5);
+        
+        expect(article_content).toEqual("la bl");
+     })
+})
