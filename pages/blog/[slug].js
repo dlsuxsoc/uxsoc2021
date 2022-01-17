05bdcda4686086a77469c784e0c1b38082036b0e
Riley <uyjustinriley@gmail.com>
2022-01-17 22:40:28 +0800

    Update [slug].js

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -4,15 +4,18 @@ import Layout from "../../components/layout";
 import SEO from "../../components/seo";
 import faker from "faker";
 import ContentfulApi from "./../api/utils/contentfulApi";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { useEffect, useState } from "react";
 
-export default function ArticlesPost({ active, title, authors, slug }) {
+export default function ArticlesPost({ active, title, authors, content, slug }) {
+    console.log ("HELLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+    console.log (content.json);
     return (
         <Layout active={-1}>
             <SEO title={"Articles Post"} />
-            <div className="w-full md:w-3/4 mt-32 mb-64 mx-auto min-h-full bg-white rounded-md shadow-xl relative z-10">
+            <div className="w-full md:w-2/3 mt-32 mb-64 mx-auto min-h-full bg-white rounded-md shadow-xl relative z-10">
                 <div className="pb-3 lg:pb-0">
-                    <h1 className="text-2xl md:text-5xl px-8 text-center text-black lg:px-32 pt-16 pb-4 md:pb-8 lg:text-left">{title}</h1>
+                    <h1 className="text-2xl md:text-5xl pl-8 lg:pl-32 pr-8 lg:pr-72 text-center text-black  pt-16 pb-4 md:pb-8 lg:text-left">{title}</h1>
                 </div>
                 <p className="italic pb-8 lg:px-32 text-center lg:text-left md:block hidden">{slug}</p>
                 <div className="pb-4 lg:pl-32 flex flex-col lg:flex-row items-center lg:items-start">
@@ -29,25 +32,8 @@ export default function ArticlesPost({ active, title, authors, slug }) {
                 </div>
 
                 <hr className="mx-4 md:mx-32"></hr>
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                
+
+                <p className="italic pb-8 lg:px-32 text-center lg:text-left md:block hidden">{documentToReactComponents(content.json)}</p>
             </div>
             <div className="fixed left-0 top-0 h-48 md:h-80 w-full lg:h-44 2xl:h-72 shadow-lg">
                 <Image
