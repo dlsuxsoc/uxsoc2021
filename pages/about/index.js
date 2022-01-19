06f86c4f549fd6c14162fbbe5b47d260651c2260
Alyssa Palmares <alyssapalmares@yahoo.com>
2022-01-20 00:56:43 +0800

    updated mobile Hero responsiveness

--- pages/about/index.js
+++ pages/about/index.js
@@ -15,7 +15,7 @@ export default function Index() {
             <SEO title={"About Us"} />
 
             {/* About UX */}
-            <section className="px-4 sm:px-32 py-2 mt-8 sm:mt-28 md:mt-8 flex flex-col-reverse lg:flex-row justify-center lg:justify-between items-center h-screen">    
+            <section className="px-4 sm:px-32 py-2 mt-28 md:mt-8 flex flex-col-reverse lg:flex-row justify-center lg:justify-between items-center h-screen">    
                 {/* Header and Text */}
                 <div className="w-full lg:w-1/2 text-center lg:text-left pr-0 lg:pr-20 pb-3 lg:pb-0 mb-8">
                     <h1 className="text-black text-3xl lg:text-5xl mb-6 lg:mb-12">UX Society</h1>
