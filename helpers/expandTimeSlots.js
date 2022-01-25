29f3fdba0bea4ca24867a1d1e5b45f9423d38104
Ivan Lim <ivalim@ymail.com>
2022-01-25 14:29:34 +0800

    cleaned up a bit

--- helpers/expandTimeSlots.js
+++ helpers/expandTimeSlots.js
@@ -2,29 +2,29 @@
 const expandTimeSlots = (timeSlots) => {
     let time = [];
     let slots = [];
-    
+    let lastIndex = 0;
     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
     time = timeSlots.split(", ")
-    for(let i = 0; i < time.length; i++){
-        time[i] = time[i].replace('M', days[0]);
-        time[i] = time[i].replace('T', days[1]);
-        time[i] = time[i].replace('W', days[2]);
-        time[i] = time[i].replace('H', days[3]);
-        time[i] = time[i].replace('F', days[4]);
-    }
-    let lastIndex = 0;
     time.map((item, index) => {
+        // replace days with their full names
+        item = item.replace('M', days[0]);
+        item = item.replace('T', days[1]);
+        item = item.replace('W', days[2]);
+        item = item.replace('H', days[3]);
+        item = item.replace('F', days[4]);
         item = item.split(/-| /);
         // console.log(slots);
+
+        // check if the times are an extension of a day
         if(!days.includes(item[0])){
             // console.log(slots[lastIndex]);
+            // add new time slots to existing day
             slots[lastIndex].times.push({
                 start: item[0],
                 end: item[1],
             })
-        }
-        else
-        {
+        }else{
+            // new day
             slots.push({
                 day: item[0],
                 times: [{
