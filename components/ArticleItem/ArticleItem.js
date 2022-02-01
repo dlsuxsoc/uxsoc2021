edbd28040ecd55f3916ad0b8eeefd9e776a98a91
Riley <uyjustinriley@gmail.com>
2022-02-01 16:07:25 +0800

    Removed unneeded offset height on article items

--- components/ArticleItem/ArticleItem.js
+++ components/ArticleItem/ArticleItem.js
@@ -46,7 +46,7 @@ const ArticleItem = ({item}) => {
     <section className="pl-4 pr-8 sm:px-32 mb-8 lg:mb-28 flex flex-col lg:flex-row justify-center items-center">
       {/* Image */}
       <div className="relative w-full ">
-        <div className= {`${styles.articleImage} w-96 h-96`}>
+        <div className= {`${styles.articleImage} w-full  md:w-96 h-96`}>
           <Image
              src={item.imagesCollection.items[0] === null ? "/images/placeholder.png" : item.imagesCollection.items[0].url}
            
@@ -62,8 +62,8 @@ const ArticleItem = ({item}) => {
       {/* Header and Text */}
       <div className="w-full lg:w-96 lg:ml-16">
         {/* Header */}
-        <div className="h-auto w-96 overflow-hidden overflow-ellipsis">
-          <h1 className="line-clamp-4 text-black break-words text-3xl lg:text-4xl mb-6 lg:mb-12 pt-8 lg:pt-0">
+        <div className="h-auto min-w-full overflow-hidden overflow-ellipsis">
+          <h1 className="line-clamp-4 text-black break-words text-3xl w-96 lg:text-4xl mb-6 lg:mb-12 pt-8 lg:pt-0">
             <Link href={`/blog/${item.slug}`}>
               <a className="hover:underline">
                 {item.title}
@@ -78,7 +78,7 @@ const ArticleItem = ({item}) => {
           <p className="text-sm lg:text-base">{DateTime.fromISO(item.date).toFormat("DDD")}</p>
         </div>
 
-        <p className="line-clamp-4 text-sm lg:text-base mb-4 h-24 break-words">{getRTFContent(item)}</p>
+        <p className="line-clamp-4 text-sm lg:text-base mb-4  break-words">{getRTFContent(item)}</p>
       </div>
     </section>
   );
