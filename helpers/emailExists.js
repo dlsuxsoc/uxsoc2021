b4e327ed3b052f5419c1215eb1852d0464f204bd
Ivan Lim <ivalim@ymail.com>
2022-02-02 15:36:30 +0800

    removed emailExists and refactored apply

--- helpers/emailExists.js
+++
@@ -1,7 +0,0 @@
-export const emailExists = (candidateEmail, emails) => {
-  console.log(emails);
-  for (let i = 0; i < emails.length; i++) {
-    if (emails[i] === candidateEmail) return true;
-  }
-  return false;
-};
