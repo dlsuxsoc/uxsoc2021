102259354e564347882cf1d1d57d5019dbcc1a6c
Ivan Lim <ivalim@ymail.com>
2022-02-02 15:42:22 +0800

    readded emailExists back

---
+++ helpers/emailExists.js
@@ -0,0 +1,7 @@
+export const emailExists = (candidateEmail, emails) => {
+    console.log(emails);
+    for (let i = 0; i < emails.length; i++) {
+      if (emails[i] === candidateEmail) return true;
+    }
+    return false;
+  };
