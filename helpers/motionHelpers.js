7a4f867ef290cc506b268edbdb4349bed6d7e744
Ben <benedict_sun@dlsu.edu.ph>
2022-03-09 21:57:29 +0800

    Made articles in home clickable
    - added animation onHover
    - removed text underline onHover
    
    - still yet to implement animation responsiveness

---
+++ helpers/motionHelpers.js
@@ -0,0 +1,10 @@
+const cardHover = {
+    scale: 1.08,
+    transition: {
+        duration: .2
+    }
+}
+
+module.exports = {
+    cardHover,
+}
--- sections/Home/Articles.js
+++ sections/Home/Articles.js
@@ -4,6 +4,8 @@ import Image from "next/image";
 import Link from "next/link";
 import getRTFContent from "../../helpers/getRTFContent";
 import styles from "../../styles/Home.module.css";
+import { motion } from "framer-motion";
+import { cardHover } from "../../helpers/motionHelpers";
 
 const Articles = ({ articles }) => {
   return (
@@ -18,63 +20,64 @@ const Articles = ({ articles }) => {
       {/* Articles Container*/}
       <section className="w-full flex flex-col lg:flex-row items-start">
         {/* Big Article */}
-        <div className="w-full lg:w-1/2 pr-0 lg:pr-20 xl:pr-28 text-center lg:text-left">
-          <div className={"relative h-48 md:h-96 lg:h-44 2xl:h-96 shadow-lg"}>
-            <Image
-              src={
-                articles[0].imagesCollection.items[0] === null
-                  ? "/images/placeholder.png"
-                  : articles[0].imagesCollection.items[0].url
-              }
-              alt={articles[0].title}
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-          <Link href={`/blog/${articles[0].slug}`}>
-            <a className="line-clamp-2  font-bold text-base md:text-xl lg:text-2xl py-2 inline-block hover:underline">
-              {articles[0].title}
+        <Link href={`/blog/${articles[0].slug}`}>
+          <motion.div className="w-full lg:w-1/2 pr-0 lg:pr-20 xl:pr-28 text-center lg:text-left cursor-pointer" whileHover={cardHover}>
+            <div className={"relative h-48 md:h-96 lg:h-44 2xl:h-96 shadow-lg"}>
+              <Image
+                src={
+                  articles[0].imagesCollection.items[0] === null
+                    ? "/images/placeholder.png"
+                    : articles[0].imagesCollection.items[0].url
+                }
+                alt={articles[0].title}
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+            <a className="line-clamp-2  font-bold text-base md:text-xl lg:text-2xl py-2 inline-block">
+                {articles[0].title}
             </a>
-          </Link>
-          <p className="break-words line-clamp-3 text-sm lg:text-base mb-4">
-            {getRTFContent(articles[0])}
-          </p>
-        </div>
+            <p className="break-words line-clamp-3 text-sm lg:text-base mb-4">
+              {getRTFContent(articles[0])}
+            </p>
+          </motion.div>
+        </Link>
 
         {/* Small Articles */}
         <div className="lg:w-1/2 lg:flex hidden flex-col items-start">
           {/* Article */}
           {articles.slice(1, 3).map((item, index) => {
             return (
-              <div
-                className="w-full pb-8 flex flex-row items-start"
-                key={index}
-              >
-                <div className={`w-1/3 relative lg:h-28 2xl:h-44 shadow-lg`}>
-                  <Image
-                    src={
-                      item.imagesCollection.items[0] === null
-                        ? "/images/placeholder.png"
-                        : item.imagesCollection.items[0].url
-                    }
-                    alt={item.title}
-                    layout="fill"
-                    objectFit="cover"
-                    objectPosition="center"
-                  />
-                </div>
+              <Link href={`/blog/${item.slug}`}>
+                <motion.div
+                  className="w-full pb-8 flex flex-row items-start cursor-pointer"
+                  key={index}
+                  whileHover={cardHover}
+                >
+                  <div className={`w-1/3 relative lg:h-28 2xl:h-44 shadow-lg`}>
+                    <Image
+                      src={
+                        item.imagesCollection.items[0] === null
+                          ? "/images/placeholder.png"
+                          : item.imagesCollection.items[0].url
+                      }
+                      alt={item.title}
+                      layout="fill"
+                      objectFit="cover"
+                      objectPosition="center"
+                    />
+                  </div>
 
-                <div className="w-2/3 flex flex-col pl-8">
-                  <Link href={`/blog/${item.slug}`}>
-                    <a className="line-clamp-2  text-base lg:text-xl mb-4 hover:underline font-bold">
+                  <div className="w-2/3 flex flex-col pl-8">
+                    <a className="line-clamp-2  text-base lg:text-xl mb-4 font-bold">
                       {item.title}
                     </a>
-                  </Link>
-                  <p className="break-words line-clamp-4 text-xs lg:text-sm">
-                    {getRTFContent(item)}
-                  </p>
-                </div>
-              </div>
+                    <p className="break-words line-clamp-4 text-xs lg:text-sm">
+                      {getRTFContent(item)}
+                    </p>
+                  </div>
+                </motion.div>
+              </Link>
             );
           })}
         </div>
