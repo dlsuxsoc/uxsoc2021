2fa5f1f0b2a9bcafc2bb681f192d8da1c0571b7f
Alyssa Palmares <alyssapalmares@yahoo.com>
2022-03-12 02:38:26 +0800

    updated Mission Vision section
    - changed to new layout
    - phone and tablet (except iPad Pro) layouts remain unchanged

--- sections/About/MissionVision.js
+++ sections/About/MissionVision.js
@@ -4,9 +4,9 @@ import Image from "next/image";
 const MissionVision = () => {
   return (
     <section className="px-4 sm:px-32 py-2 mb-16 md:mb-36 flex flex-col justify-center lg:justify-between items-center h-auto">
-      <div className="flex flex-col text-center">
+      <div className="flex flex-col lg:flex-row text-center lg:text-left items-center space-x-0 lg:space-x-8 xl:space-x-14 2xl:space-x-0">
         {/* Image */}
-        <div className="relative h-48 md:h-64 lg:h-52 2xl:h-72 mb-6 lg:mb-12">
+        <div className="w-full lg:w-1/2 relative h-48 md:h-64 lg:h-96 mb-6 lg:mb-0">
           <Image
             src="/images/mission-vision-1.png"
             alt="Placeholder-About"
@@ -16,23 +16,25 @@ const MissionVision = () => {
           />
         </div>
 
-        {/* Header and Text */}
-        <h1 className="text-black text-3xl lg:text-5xl mb-6 lg:mb-12">
-          The Mission-Vision
-        </h1>
+        <div className="w-full lg:w-1/2">
+          {/* Header and Text */}
+          <h1 className="text-black text-3xl lg:text-5xl mb-6 lg:mb-10">
+            The Mission-Vision
+          </h1>
 
-        <div className="md:w-96 mx-auto">
-          <p className="text-base lg:text-xl mb-4">
-            To create a community of Lasallian User Experience Designers and
-            enthusiasts through User Experience Education and the creation of
-            a distinct User Experience Culture within the university.
-          </p>
-          <p className="text-base lg:text-xl mb-4">
-            UX Society envisions to be DLSU's premier consulting and
-            educational body for User Experience Strategy, particularly in
-            technologies that enhance usability, guide human-computer
-            interaction, and showcase effective visual design.
-          </p>
+          <div className="md:w-96 lg:w-full mx-auto md:mx-0 space-y-4">
+            <p className="text-base lg:text-lg 2xl:text-xl">
+              To create a community of Lasallian User Experience Designers and
+              enthusiasts through User Experience Education and the creation of
+              a distinct User Experience Culture within the university.
+            </p>
+            <p className="text-base lg:text-lg 2xl:text-xl">
+              UX Society envisions to be DLSU's premier consulting and
+              educational body for User Experience Strategy, particularly in
+              technologies that enhance usability, guide human-computer
+              interaction, and showcase effective visual design.
+            </p>
+          </div>
         </div>
       </div>
     </section>
