fda02b8df71f53c51097925bb77c59d0e3abf1a6
Ben <benedict_sun@dlsu.edu.ph>
2022-03-16 18:02:50 +0800

    Revert "Added page loading bar"
    This reverts commit a665436f1008dc3d7be17bc66513397d29d5403d.

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,6 @@
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import "../styles/react-spinner/react-spinner-loader.css";
-import Router from "next/router";
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css'; //styles of nprogress
-
-// Bind Router events for onPageLoad
-Router.events.on('routeChangeStart', () => NProgress.start()); 
-Router.events.on('routeChangeComplete', () => NProgress.done()); 
-Router.events.on('routeChangeError', () => NProgress.done());  
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
