43854d52ae78e0c8cdeceaa8c5f6cb69b738c650
Ben <59037332+bensuwun@users.noreply.github.com>
2022-03-20 22:06:58 +0800

    Revert "Revert "Last checks for website launch""

--- sections/Services/Hero.js
+++ sections/Services/Hero.js
@@ -4,10 +4,13 @@ import servicesData from "../../data/services.json";
 
 const Hero = () => {
   return (
-    <section className="py-14 sm:px-8">
-      <div className="container lg:px-32 flex flex-col justify-center items-center place-items-center h-auto min-h-screen">
-        <h1 className="text-black pt-16 pb-8 text-center">Our services</h1>
-        <section className="flex flex-col lg:flex-row h-full items-center">
+    <section className="py-16">
+      <div className="container lg:px-32 flex flex-col justify-center items-center place-items-center h-full lg:justify-between">
+        {/* Header */}
+        <h1 className="text-black py-10 text-center text-4xl lg:text-6xl xl:text-5xl">Our services</h1>
+
+        {/* Service Component */}
+        <section className="mx-10 flex flex-col lg:flex-row justify-center space-y-20 lg:space-y-0 lg:space-x-8 xl:space-x-16">
           {servicesData.slice(0, 3).map((item, index) => {
             return <ServiceItem item={item} key={index} />;
           })}
