835ead73a1caaef3eb4ee478f7833e72bfc70815
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2022-04-02 14:13:32 +0800

    removed abortcontroller

--- pages/api/getSettings.js
+++ pages/api/getSettings.js
@@ -1,20 +1,28 @@
 import { notion } from "../../api/notion";
 
 const getSettings = async (req, res) => {
-    try {
-        const { results } = await notion.databases.query({ database_id: process.env.NOTION_SETTINGS });
-        var settings = {};
-        // Obtain settings
-        results.map((setting) => {
-            var data = setting.properties;
-            settings[data.Property.title[0].text.content] = data.Value.rich_text[0].text.content;
-        })
-
-        return settings;
-    } catch (e) {
-        console.error(e);
-        return e;
+  try {
+    if (process.env.NOTION_SETTINGS === undefined) {
+      return {
+        display_application_form: "No",
+      };
     }
-}
+    const { results } = await notion.databases.query({
+      database_id: process.env.NOTION_SETTINGS,
+    });
+    var settings = {};
+    // Obtain settings
+    results.map((setting) => {
+      var data = setting.properties;
+      settings[data.Property.title[0].text.content] =
+        data.Value.rich_text[0].text.content;
+    });
+
+    return settings;
+  } catch (e) {
+    console.error(e);
+    return e;
+  }
+};
 
 export default getSettings;
