f26ad8f026a6d10701286bd7908ca04c79871318
Gavin Raine Dizon <gavinrainedizon@gmail.com>
2023-02-10 21:54:02 +0800

    Added Lead Application 2023 page

---
+++ pages/api/addLeadApplication.js
@@ -0,0 +1,228 @@
+import axios from "axios";
+import { notion } from "../../api/notion";
+import { emailExists } from "../../helpers/emailExists";
+
+const addLeadApplication = async (req, res) => {
+  try {
+    const data = { ...req.body };
+
+    // Check Existing
+    const result = await notion.databases.query({
+      database_id: process.env.NOTION_LEAD_APPLICATION,
+    });
+
+    const people = result.results.map((item, index) => {
+      return item.properties["Email Address"].email;
+    });
+
+    if (emailExists(data.email, people)) {
+      const error = new Error("Duplicate Application");
+      error.code = 400;
+      error.msg = "An application with this email was already used";
+
+      throw error;
+    }
+
+    const response = await notion.pages.create({
+      parent: {
+        database_id: process.env.NOTION_LEAD_APPLICATION,
+      },
+      properties: {
+        StudentID: {
+          select: {
+            name: data.studentID !== "" ? data.studentID : "N/A",
+          },
+        },
+        Name: {
+          title: [
+            {
+              text: {
+                content: `${data.lastName.toUpperCase()}, ${data.firstName}`,
+              },
+            },
+          ],
+        },
+        Nickname: {
+          type: "rich_text",
+          rich_text: [
+            {
+              type: "text",
+              text: { content: data.nickname },
+            },
+          ],
+        },
+        "Birth Date": {
+          date: {
+            start: new Date(
+              `${data.yOB}-${String(data.mOB).padStart(2, "0")}-${String(
+                data.dOB
+              ).padStart(2, "0")}T00:00:00.000+00:00`
+            )
+              .toISOString()
+              .slice(0, 10),
+          },
+        },
+        "Preferred Pronoun": {
+          select: {
+            name: data.customPronoun !== "" ? data.customPronoun : data.pronoun,
+          },
+        },
+        "Email Address": {
+          email: data.email,
+        },
+        "Phone Number": {
+          type: "rich_text",
+          rich_text: [
+            {
+              type: "text",
+              text: { content: data.contactnum },
+            },
+          ],
+        },
+        College: {
+          select: {
+            name: data.college,
+          },
+        },
+        Program: {
+          type: "rich_text",
+          rich_text: [
+            {
+              type: "text",
+              text: { content: data.program },
+            },
+          ],
+        },
+        "Interested Department/s": {
+          multi_select:
+            data.interestedDept.map((item) => {
+              return { name: item };
+            }) || [],
+        },
+        Status: {
+          select: {
+            name: "Pending",
+          },
+        },
+      },
+      children: [
+        {
+          object: "block",
+          type: "heading_2",
+          heading_2: {
+            text: [
+              {
+                type: "text",
+                text: {
+                  content: "Pre-Interview Questions",
+                },
+              },
+            ],
+          },
+        },
+        {
+          object: "block",
+          type: "heading_3",
+          heading_3: {
+            text: [
+              {
+                type: "text",
+                text: {
+                  content: "1. What are your hobbies?",
+                },
+              },
+            ],
+          },
+        },
+        {
+          object: "block",
+          type: "paragraph",
+          paragraph: {
+            text: [
+              {
+                type: "text",
+                text: {
+                  content: data.hobbies,
+                },
+              },
+            ],
+          },
+        },
+        {
+          object: "block",
+          type: "heading_3",
+          heading_3: {
+            text: [
+              {
+                type: "text",
+                text: {
+                  content: "2. Why are you interested in becoming a lead?",
+                },
+              },
+            ],
+          },
+        },
+        {
+          object: "block",
+          type: "paragraph",
+          paragraph: {
+            text: [
+              {
+                type: "text",
+                text: {
+                  content: data.interestedOrg,
+                },
+              },
+            ],
+          },
+        },
+        {
+          object: "block",
+          type: "heading_3",
+          heading_3: {
+            text: [
+              {
+                type: "text",
+                text: {
+                  content: "3. What is user experience to you?",
+                },
+              },
+            ],
+          },
+        },
+        {
+          object: "block",
+          type: "paragraph",
+          paragraph: {
+            text: [
+              {
+                type: "text",
+                text: {
+                  content: data.whatIsUX,
+                },
+              },
+            ],
+          },
+        },
+      ],
+    });
+    console.log(res);
+
+    res.status(201).json();
+  } catch (err) {
+    //console.log(err);
+    //console.error(e);
+    res.status(err.code || 500).json(err);
+  }
+};
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "1mb",
+      externalResolver: true,
+    },
+  },
+};
+
+export default addLeadApplication;
--- pages/api/addMentorshipBooking.js
+++ pages/api/addMentorshipBooking.js
@@ -3,9 +3,6 @@ import mentorshipInstanceExists from "../../helpers/mentorshipInstanceExists";
 const addMentorshipBooking = async (req, res) => {
   try {
     const data = { ...req.body };
-    //console.log(process.env.NOTION_MENTORSHIP_BOOKING);
-    //console.log(data);
-
     // check existing
     const result = await notion.databases.query({
       database_id: process.env.NOTION_MENTORSHIP_BOOKING,
@@ -21,12 +18,12 @@ const addMentorshipBooking = async (req, res) => {
       let mentorship = await notion.pages.retrieve({
         page_id: ids[i],
       });
-    //   console.log(mentorship.properties);
+      //   console.log(mentorship.properties);
       collectionOfMentorships.push({
-          email : mentorship.properties[`Email Address`].email,
-          mentor : mentorship.properties[`Mentor`].rich_text[0].text.content,
-          date : mentorship.properties[`Date`].rich_text[0].text.content,
-        });
+        email: mentorship.properties[`Email Address`].email,
+        mentor: mentorship.properties[`Mentor`].rich_text[0].text.content,
+        date: mentorship.properties[`Date`].rich_text[0].text.content,
+      });
     }
 
     const key = {
@@ -35,17 +32,14 @@ const addMentorshipBooking = async (req, res) => {
       date: data.bookingDate + " " + data.bookingSlot,
     };
 
-    if(mentorshipInstanceExists(collectionOfMentorships, key)){
+    if (mentorshipInstanceExists(collectionOfMentorships, key)) {
       const error = new Error("Duplicate Mentorship");
       console.log("dupe");
       error.code = 400;
-      error.message = "Appointment at current date and time already exists"
+      error.message = "Appointment at current date and time already exists";
 
-      throw error
+      throw error;
     }
-
-    
-
 
     const response = await notion.pages.create({
       parent: {
