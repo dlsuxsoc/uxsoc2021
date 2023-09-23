f96c4b2e9638992e4b1154bb267c3b4e8d0b74e0
AndreiMarin15 <misael_marin@dlsu.edu.ph>
2023-09-23 22:59:37 +0800

    consoleRemove

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
 const { Client } = require("@notionhq/client");
 require("dotenv").config({
-  path: ".env.local",
+  path: "./.env.local",
 });
-
+console.log(process.env.NOTION_LEADS_PRODDB);
+console.log(process.env.NOTION_LEADS_DEVDB);
 // Initializing a client
 const notion = new Client({
   auth: process.env.NOTION_INTEGRATION_SECRET,
@@ -26,40 +27,38 @@ async function updateDb(sourceDbId, destDbId) {
 
   for (const item of source.results) {
     const properties = {
-        Name: {
-          title: [
-            {
-              text: {
-                content: item.properties.Name.title[0].text.content,
-              },
+      Name: {
+        title: [
+          {
+            text: {
+              content: item.properties.Name.title[0].text.content,
             },
-          ],
-        },
-        
-        Position: {
-          select: {
-            name: item.properties.Position.select.name,
           },
+        ],
+      },
+
+      Position: {
+        select: {
+          name: item.properties.Position.select.name,
         },
+      },
 
-        Avatar: {
-            files:  [{
-                name: item.properties.Avatar.files[0]?.name ?? "test.jpg",
-                type: item.properties.Avatar.files[0]?.type,
-                file: {
-                    url: item.properties.Avatar.files[0]?.file.url,
-                     
-                }
-            }]
-        }
-      }
+      Avatar: {
+        files: [
+          {
+            name: item.properties.Avatar.files[0]?.name ?? "test.jpg",
+            type: item.properties.Avatar.files[0]?.type,
+            file: {
+              url: item.properties.Avatar.files[0]?.file.url,
+            },
+          },
+        ],
+      },
+    };
 
-    if(item.properties.Avatar.files.length === 0) {
-
-        delete properties.Avatar
-
-      
-    }  
+    if (item.properties.Avatar.files.length === 0) {
+      delete properties.Avatar;
+    }
 
     await notion.pages.create({
       parent: {
@@ -67,8 +66,7 @@ async function updateDb(sourceDbId, destDbId) {
         database_id: destDbId,
       },
 
-      properties 
-      
+      properties,
     });
   }
 }
@@ -78,12 +76,16 @@ async function queryDb(databaseId) {
     database_id: databaseId,
   });
 
-  console.log(response.results[response.results.length-1].properties.Avatar.files[0].name)
+  // console.log(response.results[response.results.length-1].properties.Avatar.files[0].name)
   return response;
 }
 
-  deleteRecords(process.env.NOTION_LEADS_PRODDB);
+(async () => {
+  await deleteRecords(process.env.NOTION_LEADS_PRODDB);
+  await updateDb(
+    process.env.NOTION_LEADS_DEVDB,
+    process.env.NOTION_LEADS_PRODDB
+  );
+})();
 
- // queryDb(`bbbd878ff5f6489c8c39e6f54166d31e`)
-
-  updateDb(process.env.NOTION_LEADS_DEVDB, process.env.NOTION_LEADS_PRODDB)
+// queryDb(`bbbd878ff5f6489c8c39e6f54166d31e`)
