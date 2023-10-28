72bb13e86a8d40b3d5674a44d933f0713c485973
AndreiMarin15 <misael_marin@dlsu.edu.ph>
2023-10-28 22:17:33 +0800

    add domains

--- next.config.js
+++ next.config.js
@@ -8,6 +8,8 @@ module.exports = {
       "s3.us-west-2.amazonaws.com",
       "media.istockphoto.com",
       "lh3.googleusercontent.com",
+      "*.s3.us-west-2.amazonaws.com",
+      "prod-files-secure.s3.us-west-2.amazonaws.com",
     ],
     remotePatterns:[
       {
